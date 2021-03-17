5f8306200df5f96eeebd5217913de5de541b68dd
Nicolás <euclidesnoeuclides@gmail.com>
2021-03-17 16:36:36 -0500

    implementing auth and logout endpoints

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from apps.users.api.api import (
     UserAPIView, UserDetailAPIView, LoginAPIView, UserRetrieveView
 )
+from rest_framework_jwt.views import (
+    obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+)
+from rest_framework_jwt.blacklist.views import BlacklistView
 
 
 urlpatterns = [
@@ -11,5 +15,8 @@ urlpatterns = [
     path('auth/user', UserRetrieveView.as_view()),
     path('register', UserAPIView.as_view()),
     path('login', LoginAPIView.as_view()),
-    # path('logout', LogoutView.as_view(), name='knox_logout')
+    path('api-token-auth', obtain_jwt_token),
+    path('api-token-refresh', refresh_jwt_token),
+    path('api-token-verify', verify_jwt_token),
+    path("logout", BlacklistView.as_view({"post": "create"}))
 ]
--- palindrome_api/settings/base.py
+++ palindrome_api/settings/base.py
@@ -49,6 +49,7 @@ BASE_APPS = [
 THIRD_APPS = [
     'rest_framework',
     'rest_framework_jwt',
+    'rest_framework_jwt.blacklist',
     'drf_yasg'
 ]
 
